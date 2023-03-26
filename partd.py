8f7b9b89c94d2a841874003434c088bbc6e40d26
Harsh Darji <Harsh_Darji@hotmail.com>
2023-03-25 23:00:10 -0600

    Update partd.py

--- partd.py
+++ partd.py
@@ -1,3 +1,33 @@
+"""
+Aircraft Collision Data Analysis, Part D
+Description:
+    This script loads a Microsoft Excel workbook containing aircraft collision data and generates a bar chart
+showing the frequency of collisions by month.
+Author:
+    Harsh Darji
+Date:
+    24 March 2023
+Requirements:
+    - Python 3
+    - openpyxl library
+Inputs:
+    - aircraftWildlifeStrikes.xlsx: A Microsoft Excel workbook containing aircraft collision data
+Outputs:
+    - ChartForMonths worksheet: A new worksheet in the input workbook containing a bar chart showing the
+frequency of collisions by month
+Functions:
+    - get_month_cells(cells: list) -> list: Extracts the values in the Incident Month column of the input
+worksheet
+    - get_cell_values(months: list) -> list: Extracts the cell coordinates in the Incident Month column of the
+input worksheet
+    - get_cells_and_months(incident_months: list, month_cells: list) -> list: Combines the cell
+coordinates and values of the Incident Month column
+    - get_count_of_months(list_cells_months: list) -> list: Counts
+the frequency of each month in the Incident Month column
+"""
+
+
+
 from openpyxl import load_workbook
 from openpyxl.chart import Reference, BarChart
 
@@ -28,7 +58,6 @@ def get_count_of_airlines(list_cells_airlines: list) -> list:
     airlines = [list_cells_airlines[i][1]
                 for i in range(len(list_cells_airlines))]
 
-    # return set(airlines)
     return [[airline, airlines.count(airline)] for airline in set(airlines)]
 
 
@@ -45,7 +74,7 @@ print("\n\n\n")
 print(len(rows))
 print("\n\n\n")
 
-max_collisions = max(rows, key=lambda x: x[1])
+max_collisions = max(rows, key=lambda x: x[1]) # getting maximum number of collisions
 print(max_collisions)
 # print(type(max_collisions))
 
@@ -54,18 +83,18 @@ len_of_rows = len(rows)
 row_no = 0
 
 while row_no < len_of_rows:
-    if 'UNKNOWN' in rows[row_no]:
+    if 'UNKNOWN' in rows[row_no]: # deleting unknown airlines
         rows.pop(row_no)
         len_of_rows -= 1
         row_no -= 1
-    elif rows[row_no][1] < (max_collisions[1]*0.1):
+    elif rows[row_no][1] < (max_collisions[1]*0.1): # deleting flights with number of number of collisions less than 10% of maximum
         rows.pop(row_no)
         len_of_rows -= 1
         row_no -= 1
     row_no += 1
 
 
-print(rows)
+# print(rows)
 
 
 for row in rows:
@@ -84,4 +113,4 @@ chart1.add_data(data)
 chart1.set_categories(cats)
 chart1.legend = None
 sheet2.add_chart(chart1, "D2")
-workbook.save('aircraftWildlifeStrikes.xlsx')
+workbook.save('aircraftWildlifeStrikes.xlsx') # save data
